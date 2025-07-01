7bc323c5f9944ec2fade2c20e5b8b3bf8d5a3d8e
Chretien Kalala <chretienkalala22@gmail.com>
2025-07-01 03:03:56 +0100

    tailwindcss

--- main/main.js
+++ main/main.js
@@ -1,13 +1,17 @@
-const boxImage = document.getElementsByClassName("boxImage");
-const boxTitle = document.getElementsByClassName("boxTitle");
-const cards = document.getElementsByClassName("cards");
-const box_card = document.getElementsByClassName("box_card");
-const boxIngredient = document.getElementsByClassName("boxDescript");
-const modal = document.getElementById("myModal");
-const modalContent = modal.querySelector(".modal_element_content");
-//const divModalContent = document.getElementsByClassName("modal-content");
-//const btnModal = document.getElementById("openModal");
-const span = document.querySelector(".close");
+// const boxImage = document.getElementsByClassName("boxImage");
+// const boxTitle = document.getElementsByClassName("boxTitle");
+// const cards = document.getElementsByClassName("cards");
+// const box_card = document.getElementsByClassName("box_card");
+// const boxIngredient = document.getElementsByClassName("boxDescript");
+// const modal = document.getElementById("myModal");
+// const modalContent = modal.querySelector(".modal_element_content");
+// const divModalContent = document.getElementsByClassName("modal-content");
+// const btnModal = document.getElementById("openModal");
+// const span = document.querySelector(".close");
+
+const card = document.getElementsByClassName("card");
+const carousel = document.getElementsByClassName("carousel");
+const modalBox = document.getElementsByClassName("modal-box");
 
 async function getRecette() {
   // const apiKey = "5daa2e86e5614ac3aee017fec59d1af2"; // remplace par ta vraie clé
@@ -27,69 +31,72 @@ async function getRecette() {
 async function recuperationData() {
   const data = await getRecette();
 
-  // const cardsContainer = document.querySelector(".cards"); // container des cartes
-  // const modal = document.getElementById("modal"); // le modal principal
-  // const modalContent = modal.querySelector(".modal_element_content"); // zone où on injecte le contenu
-  // const closeBtn = document.getElementById("close"); // bouton pour fermer
-
   for (let i = 0; i < data.recipes.length; i++) {
     const details = data.recipes[i];
 
     console.log(details);
 
-    const card = document.createElement("div");
-    card.classList = "box_card";
+    const carouselItem = document.createElement("div");
+    carouselItem.classList = "carousel-item";
 
-    card.innerHTML = `
-      <div class="boxImage">
-        <img src="${details.image}">
-      </div>
-      <div class="boxTitle">${details.name}</div>
-      <div class="boxDescript">${details.description || ""}</div>
-      <div class="btn_detail">
-        <span>See Full Details</span>
-      </div>
+    carouselItem.innerHTML = `
+        <div class="carousel-item">
+            <div class="card bg-base-100 w-70 shadow-sm flex items-center">
+              <div class="card-body text-center">
+                <h2 class="card-title text-2xl">${details.name}</h2>
+                <p class="text-base">
+                  ${details.tags.join(", ")}
+                </p>
+              </div>
+              <figure>
+                <img 
+                  src="${details.image}"
+                  alt="${details.image}"
+                />
+              </figure>
+              <div class="mt-5 mb-5">
+                <button  class="btnShowModal bg-[#ffb300] text-white">See All Details</button>
+              </div>
+              
+            </div>
+          </div>
     `;
 
-    const btn = card.querySelector(".btn_detail");
+    const btnShowModal = carouselItem.querySelector(".btnShowModal");
 
-    btn.addEventListener("click", () => {
+    btnShowModal.addEventListener("click", () => {
       // Injecte les données dans le modal
-      modalContent.innerHTML = `
-        
-        <div class="box_modalImage"> 
-          <img src="${details.image}" alt="${details.name}">
+      modalBox.innerHTML = `
+      <dialog id="my_modal_1" class="modal">
+        <div class="modal-box">
+          <h3 class="text-lg font-bold">Hello!</h3>
+          <p class="py-4">Press ESC key or click the button below to close</p>
+          <div class="modal-action">
+            <form method="dialog">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn">Close</button>
+            </form>
+          </div>
         </div>
-        <div class = "box_Description_Recipes"> 
-        <h2>${details.name}</h2>
-        <div class="box_preparation">
-          <p>Cook Time: ${details.cookTimeMinutes} Min</p>
-          <p>Cuisine: ${details.cuisine}</p>
-          <p>Diffuculty: ${details.difficulty}</p>
-          <p>Ingredients: ${details.ingredients}</p>
-          <p>Instructions: ${details.instructions}</p>
-          <p>Rating: ${details.rating}</p>
-        </div>
-        </div>
-
+      </dialog>
       `;
       modal.style.display = "block";
     });
 
-    cards[0].appendChild(card);
+    carousel[0].appendChild(carouselItem);
   }
 
-  // Ferme le modal quand on clique sur la croix
-  span.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
+  // // Ferme le modal quand on clique sur la croix
+  // span.addEventListener("click", () => {
+  //   modal.style.display = "none";
+  // });
 
-  // Ferme le modal quand on clique en dehors du contenu
-  window.addEventListener("click", (event) => {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  });
+  // // Ferme le modal quand on clique en dehors du contenu
+  // window.addEventListener("click", (event) => {
+  //   if (event.target === modal) {
+  //     modal.style.display = "none";
+  //   }
+  // });
 }
 
 getRecette();
